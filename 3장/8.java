0b54097d3aa25e27b6eebdd4406dcb29fabef79e
Taegyeong Lee <hanarotg@naver.com>
2019-04-13 20:50:17 +0900

    수정완료

--- 3장/8.java
+++ 3장/8.java
@@ -7,14 +7,13 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
-		int i = 0;
-		int j = 0;
 		int[] arr = new int[100];
+		int j = 0;
 		
 		System.out.print("정수 몇개?>>");
 		int howManyInt = sc.nextInt();
 		
-		for(i = 0; i<howManyInt; i++) {
+		for(int i = 0; i<howManyInt; i++) {
 			// 배열에 랜덤생성한 정수를 저장
 			arr[i] = (int)(Math.random()*100 + 1);
 			
@@ -24,25 +23,25 @@ public class Main {
 				if(arr[j] == arr[i]) {
 					break;
 				}
-				
-				// if문 통해 break한 경우, j값은 i값과 같지 않으므로 이를 이용한다.
-				if(i != j) {
-					// arr[j] 값과 arr[i] 값이 다를 때까지 랜덤한 정수를 arr[j]에 저장
-					while(arr[j] == arr[i]) {
-						arr[j] = (int)(Math.random()*100 + 1);
-					}
-					
-				j = 0;
-				}
-				
-				// 10개씩 같은 줄에 원소를 출력
-				if(i%10 == 0 && i != 0) {
-					System.out.print(arr[i] + "\n");
-				} else {
-					System.out.print(arr[i] + " ");
-				}
 			}
+				
+			// if문 통해 break한 경우, j값은 i값과 같지 않으므로 이를 이용한다.
+			if(i != j) {
+				// arr[j] 값과 arr[i] 값이 다를 때까지 랜덤한 정수를 arr[j]에 저장
+				while(arr[j] == arr[i]) {
+					arr[j] = (int)(Math.random()*100 + 1);
+				}
+			
+			j = 0;
+			}
+				
+			// 10개씩 같은 줄에 원소를 출력
+			if(i%10 == 0 && i != 0) {
+				System.out.println("");
+			}
+			System.out.print(arr[i] + " ");
 		}
-			sc.close();
+		
+		sc.close();
 	}
 }
